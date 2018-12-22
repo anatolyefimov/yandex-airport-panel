24dcc75f59f30a74265944b8265e601d859fce3d
anatolyefimov <efimo.tolik@gmail.com>
2018-12-22 13:14:39 +0300

    components added

--- src/store/store.js
+++ src/store/store.js
@@ -22,7 +22,43 @@ export default class Store {
                 state[key] = value;
                 self.events.publish('stateChange', this.state);
                 self.status = 'resting';
+                return true;
             }
         })
+    }
+
+    dispatch(actionKey, payload) {
+  
+        let self = this;
+        
+        
+        if(typeof self.actions[actionKey] !== 'function') {
+          console.error(`Action "${actionKey} doesn't exist.`);
+          return false;
+        }
+        
+        console.groupCollapsed(`ACTION: ${actionKey}`);
+
+        self.status = 'action';
+        self.actions[actionKey](self, payload);
+
+        console.groupEnd();
+
+        return true;
+    }
+
+    commit(mutationKey, payload) {
+        let self = this;
+        
+        if(typeof self.mutations[mutationKey] !== 'function') {
+            console.log(`Mutation "${mutationKey}" doesn't exist`);
+            return false;
+        }
+        
+        self.status = 'mutation';
+        let newState = self.mutations[mutationKey](self.state, payload);
+        self.state = Object.assign(self.state, newState);
+
+        return true;
     }
 }
